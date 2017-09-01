e24198e8d666707108c9a34c977d63ff2c8060da
ryandelmar <30875224+ryandelmar@users.noreply.github.com>
2017-09-01 09:23:55 -1000

    Create seriesOfSurfSpots.js

---
+++ seriesOfSurfSpots.js
@@ -0,0 +1,4 @@
+var listMe = ["blackpoint","tonggs","graveyards","ricebowls","sandbar","oldmans","castles","walls","dukes","bowls"];
+for (i=0; i < listMe.length ;i++) {
+  window.alert(listMe[i]);
+}
